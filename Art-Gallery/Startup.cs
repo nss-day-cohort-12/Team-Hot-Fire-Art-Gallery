145ad14eb623879d1d991535d9b4901c97731355
Jeremy C. Swain <jeremycswain@gmail.com>
2016-05-20 10:21:45 -0500

    initial commit

---
+++ Art-Gallery/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Art_Gallery.Startup))]
+namespace Art_Gallery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
