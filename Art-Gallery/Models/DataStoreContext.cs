e63376975de3d79416fa00f9740ec8c567bc1e77
Jeremy C. Swain <jeremycswain@gmail.com>
2016-05-23 15:51:42 -0500

    set up dbcontext file

--- Art-Gallery/Models/DataStoreContext.cs
+++ Art-Gallery/Models/DataStoreContext.cs
@@ -9,10 +9,33 @@ namespace Art_Gallery.Models
     public class DataStoreContext : DbContext
     {
         public DbSet<Customer> Customer { get; set; }
+        public DbSet<ArtWork> ArtWork { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<ArtWork>()
+                .ToTable("ArtWork")
+                .HasKey(a => a.ArtWorkId);
 
+            modelBuilder.Entity<ArtShow>()
+                .ToTable("ArtShow")
+                .HasKey(s => s.ArtShowId);
+
+            modelBuilder.Entity<IndividualPiece>()
+                .ToTable("IndividualPiece")
+                .HasKey(i => i.IndividualPieceId);
+
+            modelBuilder.Entity<Invoice>()
+                .ToTable("Invoice")
+                .HasKey(n => n.InvoiceId);
+
+            modelBuilder.Entity<Customer>()
+                .ToTable("Customer")
+                .HasKey(c => c.CustomerId);
+
+            modelBuilder.Entity<Agent>()
+                .ToTable("Agent")
+                .HasKey(g => g.AgentId);
         }
     }
 }
