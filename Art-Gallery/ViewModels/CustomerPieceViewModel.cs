3f72281707ec31c115006bcddff43ebd06ffa507
Jeremy C. Swain <jeremycswain@gmail.com>
2016-05-24 15:43:04 -0500

    added customer main view with image links only

---
+++ Art-Gallery/ViewModels/CustomerPieceViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Art_Gallery.ViewModels
+{
+    public class CustomerPieceViewModel
+    {
+        public int ArtistId { get; set; }
+        public int IndividualPieceId { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public List<CustomerArtViewModel> AllPieces { get; set; }
+    }
+}
